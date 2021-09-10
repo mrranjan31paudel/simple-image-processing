Fix Derivative filters: prewitt, sobel and laplacian

laplacianmask.py
@@ -14,7 +14,7 @@ def lapmask(img):
                 q = []
                 for i in range(-1, 2):
                     for j in range(-1, 2):
-                        aas = img[h+i, w+j]*mask[i+1, j+1]
+                        aas = img[h+i, w+j, 0]*mask[i+1, j+1]
                         q.append(aas)
                 if np.sum(q) > 255:
                     s = 255
@@ -30,6 +30,9 @@ def lapmask(img):
         for wa in range(0, wi):
             img1[0][wa] = img1[1][wa]
             img1[hi-1][wa] = img1[hi-2][wa]
+        for ih in range(0, hi):
+            for iw in range(0, wi):
+                img[ih][iw] = img1[ih][iw]
 
     if ch == 3:
         for h in range(1, hi-1):
@@ -46,25 +49,25 @@ def lapmask(img):
                         q1.append(aas1)
                         q2.append(aas2)
                 if np.sum(q0) > 255:
-                    img1[h, w, 0] == 255
+                    img1[h, w, 0] = 255
                 elif np.sum(q0) < 0:
-                    img1[h, w, 0] == 0
+                    img1[h, w, 0] = 0
                 else:
-                    img1[h, w, 0] == np.sum(q0)
+                    img1[h, w, 0] = np.sum(q0)
 
                 if np.sum(q1) > 255:
-                    img1[h, w, 1] == 255
+                    img1[h, w, 1] = 255
                 elif np.sum(q1) < 0:
-                    img1[h, w, 1] == 0
+                    img1[h, w, 1] = 0
                 else:
-                    img1[h, w, 1] == np.sum(q1)
+                    img1[h, w, 1] = np.sum(q1)
 
                 if np.sum(q2) > 255:
-                    img1[h, w, 2] == 255
+                    img1[h, w, 2] = 255
                 elif np.sum(q2) < 0:
-                    img1[h, w, 2] == 0
+                    img1[h, w, 2] = 0
                 else:
-                    img1[h, w, 2] == np.sum(q2)
+                    img1[h, w, 2] = np.sum(q2)
 
         for ha in range(1, hi-1):
             img1[ha, 0, 0] = img1[ha, 1, 0]

prewittmask.py
@@ -6,8 +6,16 @@ def premask(img):
     hi, wi, ch = img.shape
     img1 = np.zeros((hi, wi, ch), np.uint8)
 
-    mask1 = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
-    mask2 = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+    mask1 = np.array([
+        [-1, -1, -1],
+        [0, 0, 0],
+        [1, 1, 1]
+    ])
+    mask2 = np.array([
+        [-1, 0, 1],
+        [-1, 0, 1],
+        [-1, 0, 1]
+    ])
 
     if ch == 1:
         for h in range(1, hi-1):
@@ -16,8 +24,8 @@ def premask(img):
                 bbs = 0
                 for i in range(-1, 2):
                     for j in range(-1, 2):
-                        aas = aas+img[h+i, w+j]*mask1[i+1, j+1]
-                        bbs = bbs+img[h+i, w+j]*mask2[i+1, j+1]
+                        aas = aas+img[h+i, w+j, 0]*mask1[i+1, j+1]
+                        bbs = bbs+img[h+i, w+j, 0]*mask2[i+1, j+1]
                 mag = np.sqrt(np.power(aas, 2)+np.power(bbs, 2))
                 if mag > 255:
                     img1[h, w] = 255
@@ -30,6 +38,9 @@ def premask(img):
         for wa in range(0, wi):
             img1[0][wa] = img1[1][wa]
             img1[hi-1][wa] = img1[hi-2][wa]
+        for ih in range(0, hi):
+            for iw in range(0, wi):
+                img[ih][iw] = img1[ih][iw]
 
     if ch == 3:
         for h in range(1, hi-1):

sobelmask.py
@@ -16,10 +16,10 @@ def sobmask(img):
                 bbs = 0
                 for i in range(-1, 2):
                     for j in range(-1, 2):
-                        aas = aas+img[h+i, w+j]*mask1[i+1, j+1]
-                        bbs = bbs+img[h+i, w+j]*mask2[i+1, j+1]
+                        aas = aas+img[h+i, w+j, 0]*mask1[i+1, j+1]
+                        bbs = bbs+img[h+i, w+j, 0]*mask2[i+1, j+1]
                 mag = np.sqrt(np.power(aas, 2)+np.power(bbs, 2))
-                print(mag)
+
                 if mag > 255:
                     img1[h, w] = 255
                 elif mag < 0:
@@ -33,6 +33,9 @@ def sobmask(img):
         for wa in range(0, wi):
             img1[0][wa] = img1[1][wa]
             img1[hi-1][wa] = img1[hi-2][wa]
+        for ih in range(0, hi):
+            for iw in range(0, wi):
+                img[ih][iw] = img1[ih][iw]
 
     if ch == 3:
         for h in range(1, hi-1):